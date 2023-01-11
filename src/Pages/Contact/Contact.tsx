8629ba26d5fcd7674eacee24111228bebbdc98a3
Mouad Anouch <md.anouchpro@gmail.com>
2023-01-11 12:42:14 +0100

    contact page style

--- src/Pages/Contact/Contact.tsx
+++ src/Pages/Contact/Contact.tsx
@@ -5,14 +5,18 @@ const Contact = () => {
   return (
     <div className={classes.container}>
         <h1>Nous contacter</h1>
-        <div className='info'>
-            <div>
+        <div className={classes.info}>
+            <div className={`${classes.contactCard} ${classes.adresse}`}>
                 <h3>Adresse</h3>
                 <p>Sophie lafitte</p>
             </div>
-            <div>
+            <div className={`${classes.contactCard} ${classes.phone}`}>
                 <h3>Numéro de téléphone</h3>
-                <a href="tel:+33632123289">+33 63 21 23 28 49</a>
+                <a href="tel:+33632123289">+33 6 21 23 28 49</a>
+            </div>
+            <div className={`${classes.contactCard} ${classes.mail}`}>
+                <h3>Email</h3>
+                <a href="mailto:hackathon@mail.com">hackathon@mail.com</a>
             </div>
         </div>
     </div>
