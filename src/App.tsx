e67c8036f663ef8d73b8e80b2048074a94c64c59
Mouad Anouch <md.anouchpro@gmail.com>
2023-01-11 10:54:20 +0100

    add QueryClient

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,21 @@
-import React from 'react';
-import logo from './assets/logo_temporary.png';
-import './App.css';
+import React from "react";
+import logo from "./assets/logo_temporary.png";
+import "./App.css";
+import { QueryClient, QueryClientProvider } from "react-query";
 
 function App() {
+  // Create a client
+  const queryClient = new QueryClient();
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Welcome to hackaton
-        </p>
-      </header>
-    </div>
+    <QueryClientProvider client={queryClient}>
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>Welcome to hackaton</p>
+        </header>
+      </div>
+    </QueryClientProvider>
   );
 }
 
