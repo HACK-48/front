4ec602b80f56a436df8737039b546203459997db
Stephane0713 <stephaned2205@gmail.com>
2023-01-29 08:25:25 +0100

    #26 base layout done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
-import "./App.css";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Home from "./pages/Home";
 import Contact from "./pages/Contact";
 import Login from "./pages/Login";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
+import { Container } from "@mui/system";
 
 function App() {
   // Create a client
@@ -12,11 +14,15 @@ function App() {
   return (
     <QueryClientProvider client={queryClient}>
       <Router>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="/login" element={<Login />} />
-        </Routes>
+        <Header />
+        <Container>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/contact" element={<Contact />} />
+            <Route path="/login" element={<Login />} />
+          </Routes>
+        </Container>
+        <Footer />
       </Router>
     </QueryClientProvider>
   );
