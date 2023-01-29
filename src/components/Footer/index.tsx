4936aee38d3aeb6c261110247a11c174d0429748
Stephane0713 <stephaned2205@gmail.com>
2023-01-29 09:25:37 +0100

    #30 done

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -3,7 +3,7 @@ import { Box, Paper, Typography } from "@mui/material";
 const Footer = () => {
   return (
     <Box sx={{ mt: "auto" }}>
-      <Paper sx={{ mt: 2, p: 2, boxShadow: 5 }}>
+      <Paper sx={{ mt: 5, p: 2, boxShadow: 5 }}>
         <Typography sx={{ py: 2 }} variant="h6" component="div" align="center">
           Footer
         </Typography>
