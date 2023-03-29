5bfcec42c312f6cbc624e315d5af8b951881f57d
Mouad Anouch <md.anouchpro@gmail.com>
2023-03-29 12:23:45 +0200

    [TASK-44]: responsive header

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,4 +1,14 @@
-import { AppBar, Toolbar, Button, IconButton, Typography } from "@mui/material";
+import { useState } from "react";
+import {
+  AppBar,
+  Toolbar,
+  Button,
+  IconButton,
+  Typography,
+  Box,
+  Menu,
+  MenuItem,
+} from "@mui/material";
 import MenuIcon from "@mui/icons-material/Menu";
 import styles from "./styles.module.scss";
 import logo from "../../assets/Rectangle 26.svg";
@@ -10,6 +20,15 @@ interface IProps {
 
 const Header = ({ isAuth = false }: IProps) => {
   const navigate = useNavigate();
+  const [menuAnchor, setMenuAnchor] = useState<null | HTMLElement>(null);
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLElement>) => {
+    setMenuAnchor(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setMenuAnchor(null);
+  };
 
   const userLinks = isAuth ? (
     <Button
@@ -30,49 +49,71 @@ const Header = ({ isAuth = false }: IProps) => {
   );
 
   return (
-    <AppBar
-      className={styles.container}
-      position="static"
-      sx={{ mb: 5, background: "black" }}
-    >
-      <Toolbar>
-        {/* <IconButton size="large" edge="start" color="inherit" aria-label="menu" sx={{ mr: 2 }}>
-          <MenuIcon />
-        </IconButton> */}
-        <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-          {/* This is a placeholder don't know if it will be replaced by logo */}
-          <img
-            onClick={() => navigate("/")}
-            className={styles.logo}
-            src={logo}
-            alt=""
-          />
-        </Typography>
-        <Button
-          onClick={() => navigate("teams")}
-          className={styles.menuLinks}
-          color="inherit"
+    <Box>
+      <AppBar
+        className={styles.container}
+        position="static"
+        sx={{ mb: 5, background: "black" }}
+      >
+        <Toolbar sx={{ display: { xs: "flex", md: "flex" }, flexWrap: "wrap" }}>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2, display: { md: "none" } }}
+            onClick={handleMenuOpen}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+            <img
+              onClick={() => navigate("/")}
+              className={styles.logo}
+              src={logo}
+              alt=""
+            />
+          </Typography>
+          <Box sx={{ display: { xs: "none", md: "flex" } }}>
+            <Button
+              onClick={() => navigate("teams")}
+              className={styles.menuLinks}
+              color="inherit"
+            >
+              Les teams
+            </Button>
+            <Button
+              onClick={() => navigate("old-projects")}
+              className={styles.menuLinks}
+              color="inherit"
+            >
+              Projet passes
+            </Button>
+            <Button
+              onClick={() => navigate("contact")}
+              className={styles.menuLinks}
+              color="inherit"
+            >
+              Contact
+            </Button>
+            {userLinks}
+          </Box>
+        </Toolbar>
+        <Menu
+          anchorEl={menuAnchor}
+          open={Boolean(menuAnchor)}
+          onClose={handleMenuClose}
         >
-          Les teams
-        </Button>
-        <Button
-          onClick={() => navigate("old-porjects")}
-          className={styles.menuLinks}
-          color="inherit"
-        >
-          Projet passes
-        </Button>
-        <Button
-          onClick={() => navigate("contact")}
-          className={styles.menuLinks}
-          color="inherit"
-        >
-          Contact
-        </Button>
-        {userLinks}
-      </Toolbar>
-    </AppBar>
+          <MenuItem onClick={() => navigate("teams")}>Les teams</MenuItem>
+          <MenuItem onClick={() => navigate("old-projects")}>
+            Projet passes
+          </MenuItem>
+          <MenuItem onClick={() => navigate("contact")}>Contact</MenuItem>
+          {userLinks}
+        </Menu>
+      </AppBar>
+    </Box>
   );
 };
-
+ 
 export default Header;
