5b3cf7a1477594f4d4c505ef653899faa083c880
Mickael DP <m.dallepasqualine@gmail.com>
2023-04-12 15:28:27 +0200

    #70 - add image partner

--- src/components/Footer/index.tsx
+++ src/components/Footer/index.tsx
@@ -1,13 +1,16 @@
 import { Box, Paper, Typography, Link, List, ListItem } from '@mui/material';
 import { Container, textAlign } from '@mui/system';
 import { Link as RouterLink } from 'react-router-dom';
-import logo from '../../assets/Rectangle 26.svg';
+import logo from '../../assets/Logo-Hack48.svg';
+import logoYnov from '../../assets/images/Rectangle 49.png';
+import logoSophia from '../../assets/images/Rectangle 50.png';
+import logoFood from '../../assets/images/Rectangle 51.png';
 
 const Footer = () => {
 	return (
 		<Box sx={{ mt: 'auto', background: 'black' }}>
-			<Container maxWidth='xl'>
-				<Box sx={{ mt: 'auto', display: 'flex', gap: 10}}>
+			<Container maxWidth='xl' sx={{ display: 'flex', position:'relative'}} >
+				<Box sx={{ mt: 'auto', display: 'flex', justifyContent:'flex-start', position:'absolute', top:'0', left:'0'}}>
 					<img
 						src={logo}
 						alt='logo'
@@ -16,9 +19,9 @@ const Footer = () => {
 						<ListItem>
 							<Link
 								component={RouterLink}
-								to={'/about'}
+								to={'/'}
 								sx={{ textDecoration: 'none', color: 'white' }}>
-								About
+								A propos
 							</Link>
 						</ListItem>
 						<ListItem>
@@ -32,16 +35,31 @@ const Footer = () => {
 						<ListItem>
 							<Link
 								component={RouterLink}
-								to={'/team'}
+								to={'/teams'}
 								sx={{ textDecoration: 'none', color: 'white' }}>
 								Team
 							</Link>
 						</ListItem>
 					</List>
 				</Box>
+				<Box sx={{ m:'auto'}}>
+					<img
+						src={logoYnov}
+						alt='logoYnov'
+					/>
+					<img
+						src={logoSophia}
+						alt='logoSophia'
+					/>
+					<img
+						src={logoFood}
+						alt='logoFood'
+					/>
+				</Box>
 			</Container>
 			<Box>
-				<Typography sx={{ textAlign: 'center', background: 'grey' }}>
+				<Typography
+					sx={{ textAlign: 'center', background: 'black', color: 'white' }}>
 					Copyright 2023 - Site crée par Hack’48 - Mentions Légales
 				</Typography>
 			</Box>
