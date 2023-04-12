91c26bb2f68f2c61cea7ff4c2c89c43eb153b59f
Mickael DP <m.dallepasqualine@gmail.com>
2023-04-12 11:43:51 +0200

    Finish contact page

--- src/pages/Contact/index.tsx
+++ src/pages/Contact/index.tsx
@@ -1,60 +1,58 @@
-import image from "../../assets/contact_image.png";
-import InstagramIcon from "@mui/icons-material/Instagram";
-import EmailIcon from "@mui/icons-material/Email";
-import AccountCircleIcon from "@mui/icons-material/AccountCircle";
-import { Box, Card, CardContent, Grid, Typography, Link } from "@mui/material";
-import { blue } from "@mui/material/colors";
+import image from '../../assets/contact_image.png';
+import InstagramIcon from '@mui/icons-material/Instagram';
+import EmailIcon from '@mui/icons-material/Email';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+import { Box, Card, CardContent, Grid, Typography, Link } from '@mui/material';
+import { blue, grey } from '@mui/material/colors';
+import ContactForm from '../../components/ContactForm';
+import logoInstagram from '../../assets/instagram-circle.png';
+import logoTiktok from '../../assets/tiktok-circle.png';
 
 const Contact = () => {
-  return (
-    <Box sx={{ display: "grid" }}>
-      <Box component="img" sx={{ justifySelf: "center" }} src={image} alt="homme d'affaire marche sur un chemin" />
-      <Typography my={4} textAlign="center" variant="h4" color="initial">
-        Contactez nous
-      </Typography>
-      <Grid container spacing={2}>
-        <Grid item xs={12} md={4}>
-          <Card variant="elevation" sx={{ height: "100%", backgroundColor: blue[50] }}>
-            <CardContent sx={{ textAlign: "center" }}>
-              <InstagramIcon sx={{ fontSize: "72px", m: 4 }} />
-              <Typography gutterBottom variant="h5" color="initial">
-                Notre Instagram
-              </Typography>
-              <Typography variant="body1">
-                <Link href="https://www.instagram.com/hack48_/" target="_blank">
-                  hack48_
-                </Link>
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Card variant="elevation" sx={{ height: "100%", backgroundColor: blue[50] }}>
-            <CardContent sx={{ textAlign: "center" }}>
-              <EmailIcon sx={{ fontSize: "72px", m: 4 }} />
-              <Typography gutterBottom variant="h5" color="initial">
-                Mail support
-              </Typography>
-              <Typography variant="body1">hack48placeholder.mail.com</Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-        <Grid item xs={12} md={4}>
-          <Card variant="elevation" sx={{ height: "100%", backgroundColor: blue[50] }}>
-            <CardContent sx={{ textAlign: "center" }}>
-              <AccountCircleIcon sx={{ fontSize: "72px", m: 4 }} />
-              <Typography gutterBottom variant="h5" color="initial">
-                Placeholder
-              </Typography>
-              <Typography variant="body1">
-                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Optio magni sint mollitia!
-              </Typography>
-            </CardContent>
-          </Card>
-        </Grid>
-      </Grid>
-    </Box>
-  );
+	return (
+		<Grid
+			container
+			rowSpacing={1}
+			columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+			<Grid
+				item
+				xs={12}
+				sm={5}
+        sx={{display: 'flex', alignItems: 'center'}}>
+				<Box >
+					<Typography
+						variant='h4'
+						component='h2'
+						fontWeight={700}>
+						Suivez-nous
+					</Typography>
+					<Box>
+						<Link sx={{mr: 2}} href='https://www.instagram.com/hack48_/'>
+							<img
+								src={logoInstagram}
+								alt='logo instagram'
+							/>
+						</Link>
+						<Link href='https://www.tiktok.com/@hack48ynov?_t=8bQhASfK9d0&_r=1'>
+							<img
+								src={logoTiktok}
+								alt='logo tiktok'
+							/>
+						</Link>
+					</Box>
+					<Typography variant='body1' sx={{color: 'grey'}}>
+						Copyright 2023 - Site crée par Hack’48 - Mentions Légales
+					</Typography>
+				</Box>
+			</Grid>
+			<Grid
+				item
+				xs={12}
+				sm={7}>
+				<ContactForm />
+			</Grid>
+		</Grid>
+	);
 };
 
 export default Contact;
