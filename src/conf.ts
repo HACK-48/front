d7ceb8d91cd3a15df5f55b9a9cc3675b1a651235
Stephane0713 <67142299+Stephane0713@users.noreply.github.com>
2023-05-10 14:27:08 +0200

    Update conf.ts

--- src/conf.ts
+++ src/conf.ts
@@ -1,2 +1,2 @@
-// export const API_URL = "https://hack48-api.osc-fr1.scalingo.io/api/v1";
-export const API_URL = "http://localhost:8080/api/v1";
+export const API_URL = "https://hack48-api.osc-fr1.scalingo.io/api/v1";
+// export const API_URL = "http://localhost:8080/api/v1";
